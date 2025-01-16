a62c9ebaf5c92c818e5b8aef74a35397a9e4ef7d
huyunbo <huyunbo@sunmi.com>
2025-01-16 19:35:15 +0800

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3003
-  }
+    port: 3003,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // 后端服务地址
+        changeOrigin: true, // 如果后端需要匹配Origin
+        // rewrite: (path) => path.replace(/^\/api/, '/api123/'), // 如果需要修改路径
+        rewrite: (path) => path, // 如果需要修改路径
+        logLevel: 'debug', // 打开代理日志
+      },
 
-})
+    },
+  },
+});
